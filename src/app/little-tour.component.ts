49ff95ef8274c9e2bbf2873c2c6d39ec5dfb2429
sorvinvl <sorvinvl@gmail.com>
2017-04-10 20:13:16 +0500

    Original

--- src/app/little-tour.component.ts
+++ src/app/little-tour.component.ts
@@ -8,20 +8,27 @@ let newHero: Number=0;
     <td *ngFor="let her of heroes">{{her}}</td>
   </tr>
 </table><br>
-    <input type="number" min="10" value="10" max="99" style="color: green; font-size: 2em" #newHero
+    <input type="number" min="1" value="5" max="9" style="color: green; font-size: 2em" #newHero
      (keyup.enter)="addHero(newHero.value)"/>
-    <button style="color: green; font-size: 2em" (click)="addHero(newHero.value)">+</button>
+    <button style="color: green; font-size: 2em" (click)="addHeroRnd()">+??</button>
 `
 })
 export class LittleTourComponent {
-  heroes = [10, 15, 12, 23, 32, 23, 45, 68];
-  addHero(newHero: number) {
-let OutString = this.heroes.length;
+  heroes = [1, 5, 2, 3, 2, 8, 4, 6, 9];
+  addHero(newHero: number)
+ { let Rnd = this.heroes.length;
     if (newHero >0 ) 
     {
-      this.heroes.push(newHero/1);
+      this.heroes.push(newHero);
+      Rnd=Math.round(Math.random()*8+1);
       //this.heroes.push(this.heroes.length);
-      //console.log(this.heroes, newHero,this.heroes.length);
+      console.log(newHero,this.heroes[this.heroes.length-1]);
     }
   }
+  addHeroRnd()
+ { let  Rnd=Math.round(Math.random()*8+1);
+    if (Rnd >0) this.heroes.push(Rnd);
+    console.log(Rnd, Rnd.toFixed(1),this.heroes[this.heroes.length-1]);
+  }
+  
 }
