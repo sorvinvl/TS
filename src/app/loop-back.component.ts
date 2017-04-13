e95f89308ab05452e028fe7c8546d68b6f236fad
sorvinvl <sorvinvl@gmail.com>
2017-04-13 20:05:24 +0500

    Original

--- src/app/loop-back.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'loop-back',
-  template: `
-    <input #box (keydown)="0">
-    <p>{{box.value}}</p>
-  `
-})
-export class LoopbackComponent { }
